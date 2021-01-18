1ec2c6287fe35f2b09e672c6d3e345cc74bbfc26
dwilicious <65534615+dwilicious@users.noreply.github.com>
2021-01-19 06:29:47 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+#Test the answer
+
+import your_answer;
+
+def test_your_answer():
+    assert your_answer.find_message("How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO"
+    assert your_answer.find_message("hello world!") == ""
+    assert your_answer.find_message("HELLO WORLD!!!") == "HELLOWORLD"
